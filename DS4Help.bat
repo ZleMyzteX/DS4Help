9e03d651f3b386cce81ee57f72e38dbef2b54db4
ZleMyzteX <zlemyztex@pm.me>
2021-04-02 00:36:30 +0200

    Added service check
    * script now checks if service is running before attempting to stop it
    * minor changes to output

--- DS4Help.bat
+++ DS4Help.bat
@@ -7,14 +7,14 @@ title DS4Help
 
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
-echo [35m[DS4Help][0m This script stops all processes that block DS4Windows from using exclusive mode. 
-echo [35m[DS4Help][0m Press [32many key[0m to start stopping Game processes and Nvidia services.
+echo [35m[DS4Help][0m this script stops all processes that block DS4Windows from using exclusive mode. 
+echo [35m[DS4Help][0m press [32many key[0m to start stopping the processes.
 
 pause > NUL
 :: my approach is to simply try to kill every task, without checking if the task is running - could be done better for sure
 :: it works like this though.
 :: origin 
-@echo [35m[DS4Help][0m stopping gamelaunchers [Origin, Steam, Epic, Utrash]
+@echo [35m[DS4Help][0m stopping gamelaunchers [origin, steam, epic, uplay]
 taskkill /f /im origin.exe
 :: steam processes
 taskkill /f /im steam.exe 
@@ -27,7 +27,7 @@ taskkill /f /im UplayWebCore.exe
 ::epic 
 taskkill /f /im EpicGamesLauncher.exe
 
-@echo [35m[DS4Help][0m stopping Edge, Windows Store, Netflix and the gamebar. 
+@echo [35m[DS4Help][0m stopping edge, windows store, netflix and the xbox gamebar. 
 ::misc
 taskkill /f /im msedge.exe /t
 taskkill /f /im WinStore.App.exe
@@ -35,18 +35,33 @@ taskkill /f /im WWAHost.exe
 taskkill /f /im gamebar.exe
 
 @echo [35m[DS4Help][0m all processes should be dead / were dead already. 
-@echo [35m[DS4Help][0m stopping nvidia services. 
+@echo [35m[DS4Help][0m checking for nvidia services.
 
-net stop NvContainerLocalSystem 
-net stop NVDisplay.ContainerLocalSystem
+sc query NvContainerLocalSystem | find "RUNNING"
+if "%ERRORLEVEL%"=="0" (
+    @echo [35m[DS4Help][0m NvContainerLocalSystem is running, stopping it...
+	net stop NvContainerLocalSystem
+) else (
+    @echo [35m[DS4Help][0m NvContainerLocalSystem is already stopped. 
+)
 
-@echo [35m[DS4Help][0m Services should be dead, check if controller is in exclusive mode. 
+sc query NVDisplay.ContainerLocalSystem | find "RUNNING"
+if "%ERRORLEVEL%"=="0" (
+    @echo [35m[DS4Help][0m NVDisplay.ContainerLocalSystem is running, stopping it...
+	net stop NVDisplay.ContainerLocalSystem
+) else (
+    @echo [35m[DS4Help][0m NVDisplay.ContainerLocalSystem is already stopped. 
+)
+
+
+@echo [35m[DS4Help][0m services should be dead, check if controller is in exclusive mode. 
 @echo [35m[DS4Help][0m if exclusive mode worked, press a key to restart nvidia services.
 
 pause > NUL 
 
 net start NvContainerLocalSystem
 net start NVDisplay.ContainerLocalSystem
+
 @echo [35m[DS4Help][0m services should be running again.
 @echo [35m[DS4Help][0m thanks for using DS4Help - made by zle
 @echo [35m[DS4Help][0m press any button to exit.
